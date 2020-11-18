c57cf835165a94834253f44e4db5118af225e71d
user <>
2020-11-18 13:36:55 -0500

    game

---
+++ app/src/main/java/ca/yorku/caps/Game.java
@@ -0,0 +1,16 @@
+package ca.yorku.caps;
+
+import ca.roumani.i2c.CountryDB;
+
+public class Game
+{
+    private CountryDB db;
+
+    public Game()
+    {
+        this.db = new CountryDB();
+    }
+{
+
+}
+}
