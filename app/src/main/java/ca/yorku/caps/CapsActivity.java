07572bb5765b20fed977dfe65228715b2ed8bd1a
EECS User <user@wsc106-08.eecs.yorku.ca>
2020-11-18 13:29:59 -0500

    Initial commit

---
+++ app/src/main/java/ca/yorku/caps/CapsActivity.java
@@ -0,0 +1,16 @@
+package ca.yorku.caps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CapsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caps_layout);
+    }
+}
